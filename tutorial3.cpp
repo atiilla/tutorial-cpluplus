05b32a1995cb87e4fd1e17583e2f9918529df990
injecti0n <9992685+injecti0n@users.noreply.github.com>
2022-04-25 01:23:07 +0200

    tutorial3

---
+++ tutorial3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+using std::string;
+using std::cout;
+using std::cin;
+
+int main()
+{   
+    int num1, num2;
+    cout << "Enter first number ";
+    cin >> num1;
+    cout << "Enter second number ";
+    cin >> num2;
+    cout << "The sum of two numbers is " << num1 + num2 << endl;
+    // ############################################################
+
+    string fullname;
+    cout << "Enter your full name: ";
+
+    std::cout << "test omitting namespace";
+
+    return 0;
+}
