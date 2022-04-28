4a6512176ccbb24f96addbd6ffb1e42f682a122f
injecti0n <9992685+injecti0n@users.noreply.github.com>
2022-04-29 00:43:58 +0200

    odd even numbers c++

--- tutorial4.cpp
+++ tutorial4.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 using std::cout;
 int numbers[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+string names[10] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 void myFunction()
 {
     cout << "Hello World!" << endl;
@@ -24,6 +25,27 @@ void NumbersPrint(int arr[], int size){
     }
 }
 
+void StringsPrint(string arr[], int size){
+    for(int i = 0; i < 10; i++){
+        cout << arr[i] << endl;
+    }
+}
+
+void OddNumbersPrint(int arr[], int size){
+    for(int i = 0; i < 10; i++){
+        if(arr[i] % 2 == 1){
+            cout << arr[i] << endl;
+        }
+    }
+}
+
+void EvenNumbersPrint(int arr[], int size){
+    for(int i = 0; i < 10; i++){
+        if(arr[i] % 2 == 0){
+            cout << arr[i] << endl;
+        }
+    }
+}
 
 
 int main(){
@@ -31,5 +53,8 @@ int main(){
     myFunction2(10, 20);
     CallbackParam(10, 20, myFunction2);
     NumbersPrint(numbers, 10);
+    StringsPrint(names, 10);
+    EvenNumbersPrint(numbers, 10);
+    OddNumbersPrint(numbers, 10);
     return 0;
 }
