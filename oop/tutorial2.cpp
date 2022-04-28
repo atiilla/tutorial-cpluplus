5b22f266406236a7ee42bd20c7612a85038b1f6b
injecti0n <9992685+injecti0n@users.noreply.github.com>
2022-04-29 01:14:06 +0200

    file io

--- oop/tutorial2.cpp
+++ oop/tutorial2.cpp
@@ -1,5 +1,6 @@
 // object oriented programming tutorial 2
 #include <iostream>
+#include <fstream>
 using namespace std;
 using std::cout;
 using std::string;
@@ -10,12 +11,62 @@ public:
     string name;
     void getFullName();
     void printArrayItems(int arr[], int size);
+    void readFile(string filename);
+    void writeFile(string filename);
+    void appendFile(string filename);
 };
 
 // function defined outside of class
 void MyClass::getFullName()
 {
     cout << "Full Name: " << this->name << endl;
+}
+
+void MyClass::readFile(string filename)
+{
+    ifstream myfile(filename);
+    string line;
+    if (myfile.is_open())
+    {
+        while (getline(myfile, line))
+        {
+            cout << line << '\n';
+        }
+        myfile.close();
+    }
+    else
+    {
+        cout << "Unable to open file";
+    }
+}
+
+void MyClass::appendFile(string filename)
+{
+    ofstream myfile;
+    myfile.open(filename, ios::app);
+    if (myfile.is_open())
+    {
+        myfile << "This is a new line" << endl;
+        myfile.close();
+    }
+    else
+    {
+        cout << "Unable to open file";
+    }
+}
+
+void MyClass::writeFile(string filename)
+{
+    ofstream myfile(filename);
+    if (myfile.is_open())
+    {
+        myfile << "Test message \n";
+        myfile.close();
+    }
+    else
+    {
+        cout << "Unable to open file";
+    }
 }
 
 void MyClass::printArrayItems(int arr[], int size){
@@ -32,5 +83,8 @@ int main()
     myObj.getFullName(); // method call
     int numbers[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     myObj.printArrayItems(numbers, 10);
+    myObj.readFile("readme.txt");
+    myObj.writeFile("write.txt");
+    myObj.appendFile("write.txt");
     return 0;
 }
