1f99936bdc301344b18992bbe7e568f37b754625
injecti0n <9992685+injecti0n@users.noreply.github.com>
2022-04-29 19:37:49 +0200

    updated

--- oop/Expenses.cpp
+++ oop/Expenses.cpp
@@ -2,10 +2,90 @@
 
 #include <iostream>
 using namespace std;
+using std::cin;
+#define GREEN "\033[32m"  /* Green */
+#define RED "\033[31m"    /* Red */
+#define YELLOW "\033[33m" /* Yellow */
+#define RESET "\033[0m"
+
+class Budget
+{
+public:
+    int total_budget = 0;
+    int total_expenses = 0;
+    int total_remaining = 0;
+
+    Budget(int total_budget, int total_expenses, int total_remaining)
+    {
+        this->total_budget = total_budget;
+        this->total_expenses = total_expenses;
+        this->total_remaining = total_remaining;
+    }
+
+    void set_budget(int budget)
+    {
+        total_budget = budget;
+    }
+
+    void set_expenses(int expenses)
+    {
+        total_expenses = expenses;
+    }
+
+    void set_remaining(int remaining)
+    {
+        total_remaining = remaining;
+    }
+
+    int get_budget()
+    {
+        return total_budget;
+    }
+
+    int get_expenses()
+    {
+        return total_expenses;
+    }
+
+    int get_remaining()
+    {
+        return total_remaining;
+    }
+
+    void print_budget()
+    {
+        cout << YELLOW << "Budget: " << total_budget << RESET << endl;
+    }
+
+    void print_expenses()
+    {
+        cout << YELLOW << "Expenses: " << total_expenses << RESET << endl;
+    }
+
+    void print_remaining()
+    {
+        cout << YELLOW << "Remaining: " << total_remaining << RESET << endl;
+    }
+
+    void print_total()
+    {
+        cout << YELLOW << "Total: " << total_budget - total_expenses << RESET << endl;
+    }
+
+    void print_all()
+    {
+        print_budget();
+        print_expenses();
+        print_remaining();
+        print_total();
+    }
+};
 
 int main()
 {
     int choice;
+    int money;
+    Budget budget(0, 0, 0);
     while (true)
     {
         cout << "1. Add Expenses" << endl;
@@ -18,20 +98,24 @@ int main()
         {
         case 1:
             cout << "expenses" << endl;
+
             break;
         case 2:
-            cout << "save money" << endl;
+            cout << YELLOW << "How much do you want add?" << RESET << endl;
+            cin >> money;
+            budget.set_budget(money);
+            cout << YELLOW << budget.get_budget() << " amount added " << RESET << endl;
             break;
         case 3:
-            cout << "Total budget" << endl;
+            budget.print_budget();
             break;
         case 4:
             cout << "Exit" << endl;
-            return 0;
+            exit(1);
         default:
             cout << "Invalid Choice" << endl;
             break;
         }
     }
-     return 0;
+    return 0;
 }
